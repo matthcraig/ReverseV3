6267225fe2ef8aeed3d4abdb7399e26b6e1bbabe
MatthewCraig <mcraig@bizlibrary.com>
2023-03-13 23:55:21 -0700

    added a walrus

--- main.js
+++ main.js
@@ -5,22 +5,32 @@ function reverse(str) {
 
 //make some other thingys that reverse some strings
 function reverseV2(string){
+
     if (string === '') {
         return '';
     } else { 
-        let res = reverseV2(string.substr(1)) + string.charAt(0);
-        return res;
+        let result = reverseV2(string.substr(1)) + string.charAt(0);
+        return result;
     }
 }
+
 
 //loop
 function reverseV3(string) {
     let newString = '';
     for (let i = string.length -1; i >= 0; i--) {
-        newString += string[i]
+        newString += string[i] 
     }
     return newString
 }
+
+
+$(document).ready(function() {
+    $(".walrus-showing").click(function() {
+        console.log('hello')
+        $(".hidden").toggle();
+    })
+})
 
 //clear click
 $(document).ready(function() {
